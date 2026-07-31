ba8b96d4f4b2ad6ccd8735049d2abca5c7f010c9
nopalss <holanakdo@gmail.com>
2026-07-31 14:51:43 +0700

    fix: disable caching on 403 redirect & update 403 meta tags

--- controllers/request/maintenance/index.php
+++ controllers/request/maintenance/index.php
@@ -1,5 +1,8 @@
 ﻿<?php
 // Security: Prevent direct directory browsing
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 http_response_code(403);
 header("Location: /cms/error/403.html");
 exit;
